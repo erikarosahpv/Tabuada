86a6af99fabf85823e2977672e2645af00ffbd4d
erikarosapv <erikarosamgalternativo@gmail.com>
2023-07-23 21:31:25 -0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplication");
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        const template =`<div class="row">
+            <div class="operations">${number} x ${i} =</div>
+            <div class="result">${result}</div>
+        </div>`
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerText = number;
+}
+
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber,multiplicatorNumber)
+})
